70c1185537917bccc80d275110bd60a641d1ce2d
Magdalena Redka <magda.redka@gmail.com>
2024-10-21 00:49:14 +0000

    Create add_secret_ingredient function to add new secret ingredients to the recipe book

--- run.py
+++ run.py
@@ -15,6 +15,19 @@ def add_new_recipe():
     recipe[name] = {"Ingredients": ingredients, "Instructions": instructions}
     print(f"Recipe '{name}' added successfully.")
 
+def add_secret_ingredient():
+    """
+    Add the secret ingredient to the chosen recipe
+    """
+    name = input("Enter the recipe name to add secret ingredient to: ").strip()
+
+    if name in recipe:
+        secret_ingredient = input("Enter the secret ingredient: ").strip()
+        recipe[name]["Ingredients"].append(secret_ingredient)
+        print(f"Ingredient '{secret_ingredient}' added to recipe '{name}'.")
+    else:
+        print(f"'{name}' not found in the Chef Secret Recipes.")
+
 def main():
     """
     Main function to display user options
@@ -22,10 +35,16 @@ def main():
     while True:
         print("\nChef Secret Recipes Book:")
         print("1. Add a new recipe")
+        print("2. Add a secret ingredient to a recipe")
 
         choice = input("Choose one option: ").strip()
         if choice == "1":
             add_new_recipe()
+        elif choice == "2":
+            add_secret_ingredient()
+            break
+        else:
+            print("Invalid choice, please try again.")
 
 # Call the main function
 if __name__ == "__main__":
